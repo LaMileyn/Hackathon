7fd4aee283091a2570d9e5b4f1a3bd84893a1f37
Volg <lost-is-died@mail.ru>
2022-11-13 14:47:40 +0500

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import AuthPage from "./views/auth";
 import './assets/styles/main.scss'
 import ChatPage from "./views/chat";
 import {RequireAuth} from "./utils/hocs/requireAuth";
 import MainLayout from "./components/MainLayout/MainLayout";
+import {useAppSelector} from "./utils/hooks";
 
 
 const App = () => {
+
+    const dialogId = useAppSelector( state => state.chat.dialogId )
+
     return (
         <div className="App">
             <Routes>
                 <Route path="/auth" element={<AuthPage/>}/>
                 <Route path="/" element={<MainLayout/>}>
+                    <Route index element={<Navigate to={`chat/${dialogId}`}/>}/>
                     <Route path="chat/:id" element={
                         <RequireAuth>
                             <ChatPage/>
